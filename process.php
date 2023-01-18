2898c194651429613fba8c244e9c64b8acd07feb
stephentheofanous <42064389+stephentheofanous@users.noreply.github.com>
2023-01-18 12:20:01 +0000

    hostinger_tx_and_contact_form

---
+++ process.php
@@ -0,0 +1,42 @@
+<?php
+$to      = 'jazzlanterns@gmail.com';
+$from    = $_POST['email'];
+$email   = $_POST['email'];
+$subject = 'Received New Event Enquiry';
+$message = "Name: ".$_POST['name']."\nEmail: ".$_POST['email']."\nEvent date: ".$_POST['date']."\nMessage: ".$_POST["message"]."";
+$headers = 'From: '.$from."\r\n" .
+        'Reply-To: '.$email."\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+
+if(mail($to, $subject, $message, $headers))
+{
+	// resend
+
+$to1      = $_POST['email'];
+$from1    = 'jazzlanterns@gmail.com';
+$email1   = "jazzlanterns@gmail.com";
+$subject1 = 'Jazz Lanterns';
+$message1 = "Thank you for getting in touch! We will respond to your message as soon as we can.\n\nName: ".$_POST['name']."\nEmail: ".$_POST['email']."\nEvent date: ".$_POST['date']."\nMessage: ".$_POST["message"]."";
+$headers1 = 'From: '.$from1."\r\n" .
+        'Reply-To: '.$email1."\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+
+if(mail($to1, $subject1, $message1, $headers1))
+{
+	echo'<script>$("#form").hide()</script>';
+	echo'<div class="alert alert-success"><strong>Thank you for your message,<br>we will be in touch.</strong></div>';
+	echo'<script>$("#reset")[0].reset()</script>';
+}
+else
+{
+	echo'<script>swal("oops", "confirmation email not sent!", "info")</script>';
+}
+
+
+	// end resend
+}
+else
+{
+	echo'<script>swal("oops", "email not sent!", "info")</script>';
+}
+?>
