bda515914490e9e19781ca69cb3aabdf1b1f235c
stephentheofanous <42064389+stephentheofanous@users.noreply.github.com>
2023-03-17 18:02:57 +0000

    BIG REDO

--- js/grayscale.js
+++ js/grayscale.js
@@ -23,20 +23,17 @@
   // Activate scrollspy to add active class to navbar items on scroll
   $('body').scrollspy({
     target: '#mainNav',
-    offset: 100 
+    offset: 100
   });
 
-  // MY SCROLL TRIGGERS
+  // Collapse Navbar
   var navbarCollapse = function() {
-    if ($("#mainNav").offset().top > 150) {
-      $("#mainNav").addClass("navbar-shrink"); 
-      // $(glow);
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
     } else {
-      $("#mainNav").removeClass("navbar-shrink"); 
-      // $(dft);
+      $("#mainNav").removeClass("navbar-shrink");
     }
   };
-
   // Collapse now if page is not at top
   navbarCollapse();
   // Collapse the navbar when page is scrolled
